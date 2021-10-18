30d57904045dd8c8860567672d22340d953da5a4
ShaXx <sop.ugc2@gmail.com>
2021-10-18 12:53:10 +0500

    fix: enable cors

--- import-service/serverless.ts
+++ import-service/serverless.ts
@@ -83,9 +83,34 @@ const serverlessConfiguration: AWS = {
           },
         }
       },
+      GatewayResponseDefault4XX: {
+        Type: "AWS::ApiGateway::GatewayResponse",
+        Properties: {
+          ResponseParameters: {
+            "gatewayresponse.header.Access-Control-Allow-Origin": "'*'",
+            "gatewayresponse.header.Access-Control-Allow-Headers": "'*'"
+          },
+          ResponseType: "DEFAULT_4XX",
+          RestApiId: {
+            Ref: "ApiGatewayRestApi"
+          }
+        }
+      },
+      GatewayResponseDefault5XX: {
+        Type: "AWS::ApiGateway::GatewayResponse",
+        Properties: {
+          ResponseParameters: {
+            "gatewayresponse.header.Access-Control-Allow-Origin": "'*'",
+            "gatewayresponse.header.Access-Control-Allow-Headers": "'*'"
+          },
+          ResponseType: "DEFAULT_5XX",
+          RestApiId: {
+            Ref: "ApiGatewayRestApi"
+          }
+        }
+      }
     }
   },
-
   // import the function via paths
   functions: { importProductsFile, importFileParser, catalogBatchProcess },
 };
